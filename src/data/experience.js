a8960cf1472c096ebd06bed74e63f0f19b79d5a8
res Arfita <hresaarfita@gmail.com>
2025-02-04 09:48:19 +0700

    add cards and data on page Experience

---
+++ src/data/experience.js
@@ -0,0 +1,6 @@
+const experience = [
+    { id: 1, role: "Full-stack Developer", description: "lorem ipsum.. ", year: "2024", month: "Aug - Nov", image: ""},
+    { id: 2, role: "Web Developer", description: "lorem ipsum.. ", year: "2024", month: "Jan - Present", image: ""},
+]
+
+export default experience;
